294bcf02eb0692432dee65bd25a7dada5747ef1f
Jonatan Zint <jonatan@ctrl.alt.coop>
2022-04-28 09:19:41 +0200

    Add translations and friendly action strings for audit log

--- pretalx_hitalx/signals.py
+++ pretalx_hitalx/signals.py
@@ -1,11 +1,10 @@
 # Register your receivers here
-from django.db.models import Sum
 from django.dispatch import receiver
 from django.urls import resolve, reverse
 from django.utils.translation import ugettext as _
 
+from pretalx.common.signals import activitylog_display
 from pretalx.orga.signals import nav_event
-from pretalx.person.models import User
 
 
 @receiver(nav_event, dispatch_uid='speakers_expenses')
@@ -20,3 +19,15 @@ def navbar_info(sender, request, **kwargs):
         'active': url.namespace == 'plugins:pretalx_hitalx' and url.url_name == 'speakers_by_expense.view',
     }]
 
+
+PLUGIN_ACTION_TYPES = {
+    'pretalx_hitalx.expense_item.edit': _('Speaker expense was edited'),
+    'pretalx_hitalx.expense_item.create': _('Speaker expense was created'),
+    'pretalx_hitalx.expense_item.mark': _('Expense paid status was changed')
+}
+
+
+@receiver(activitylog_display)
+def default_activitylog_display(sender, activitylog, **kwargs):
+    if activitylog.action_type in PLUGIN_ACTION_TYPES:
+        return f'{PLUGIN_ACTION_TYPES[activitylog.action_type]} ({activitylog.data})'
--- pretalx_hitalx/templatetags/pretalx_hitalx.py
+++ pretalx_hitalx/templatetags/pretalx_hitalx.py
@@ -11,3 +11,9 @@ def total_expenses(user):
     amount = Decimal(user.expenses.all().aggregate(Sum('amount'))['amount__sum'] or 0)
     return f'{amount:.2f} €'.replace('.', ',')
 
+
+@register.filter(name='total_expenses')
+def paid_expenses(user):
+    amount = Decimal(user.expenses.filter(paid=True).aggregate(Sum('amount'))['amount__sum'] or 0)
+    return f'{amount:.2f} €'.replace('.', ',')
+
