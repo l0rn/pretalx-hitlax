0f41060d5dce0d6d70ca1816821783621dac2b4a
Jonatan Zint <jonatan@ctrl.alt.coop>
2022-05-04 09:20:39 +0200

    Chronological order for tour selection, add passenger selection in tour form

--- pretalx_hitalx/form.py
+++ pretalx_hitalx/form.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.forms import CharField, ModelForm
 from django_scopes.forms import SafeModelMultipleChoiceField
+from django_scopes import scope
 from pretalx.common.mixins.forms import ReadOnlyFlag
+from pretalx.event.models import Event
 from pretalx.person.models import SpeakerProfile
 
 from .models import ExpenseItem
@@ -24,8 +26,8 @@ class SpeakerExpenseForm(ReadOnlyFlag, ModelForm):
 
 class SpeakerToursForm(ReadOnlyFlag, ModelForm):
     tours = forms.ModelMultipleChoiceField(
-         queryset=Tour.objects.all(),
-         widget=forms.SelectMultiple(attrs={"class": "select2"}),
+        queryset=Tour.objects.all().order_by('departure_time'),
+        widget=forms.SelectMultiple(attrs={"class": "select2"}),
     )
 
     def save(self, *args, **kwargs):
@@ -45,16 +47,32 @@ class SpeakerToursForm(ReadOnlyFlag, ModelForm):
         }
 
 
+class PassengerChoiceField(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return obj.user.get_display_name()
+
+
 class TourForm(ReadOnlyFlag, ModelForm):
     description = CharField()
     start_location = CharField()
     departure_time = forms.DateTimeField()
+    passengers = PassengerChoiceField(
+        queryset=SpeakerProfile.objects.none(),
+        widget=forms.SelectMultiple(attrs={"class": "select2"}),
+        blank=True,
+        required=False
+    )
 
     def __init__(self, *args, user=None, locales=None, organiser=None, **kwargs):
         super().__init__(*args, **kwargs)
+        with scope(event=Event.objects.get(id=kwargs['initial'].get('event', None))):
+            self.fields["passengers"].queryset = SpeakerProfile.objects.all()
         self.fields["departure_time"].widget.attrs["class"] = "datetimepickerfield"
 
     class Meta:
         model = Tour
-        fields = ["description", "departure_time", "start_location", "type", "event"]
-        widgets = {'event': forms.HiddenInput()}
+        fields = ["description", "departure_time", "start_location", "type", "event", "passengers"]
+        widgets = {'event': forms.HiddenInput(), 'passengers': forms.SelectMultiple()}
+        field_classes = {
+            "passengers": SafeModelMultipleChoiceField,
+        }
